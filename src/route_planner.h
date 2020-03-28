a7d113a4624fb86ca1398067bee3002e486a51d4
Maurice Loskyll <maurice@loskyll.de>
2020-03-28 20:01:52 +0100

    Fill ToDos and clang format

--- src/route_planner.h
+++ src/route_planner.h
@@ -2,32 +2,32 @@
 #define ROUTE_PLANNER_H
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include "route_model.h"
 
-
-class RoutePlanner {
+class RoutePlanner
+{
   public:
-    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+    RoutePlanner(RouteModel& model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
-    float GetDistance() const {return distance;}
+    float GetDistance() const { return distance; }
     void AStarSearch();
 
     // The following methods have been made public so we can test them individually.
-    void AddNeighbors(RouteModel::Node *current_node);
-    float CalculateHValue(RouteModel::Node const *node);
-    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
-    RouteModel::Node *NextNode();
+    void AddNeighbors(RouteModel::Node* current_node);
+    float CalculateHValue(const RouteModel::Node* node);
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node*);
+    RouteModel::Node* NextNode();
 
   private:
     // Add private variables or methods declarations here.
     std::vector<RouteModel::Node*> open_list;
-    RouteModel::Node *start_node;
-    RouteModel::Node *end_node;
+    RouteModel::Node* start_node;
+    RouteModel::Node* end_node;
 
     float distance = 0.0f;
-    RouteModel &m_Model;
+    RouteModel& m_Model;
 };
 
 #endif
--- test/utest_rp_a_star_search.cpp
+++ test/utest_rp_a_star_search.cpp
@@ -1,35 +1,38 @@
-#include "gtest/gtest.h"
 #include <fstream>
 #include <iostream>
 #include <optional>
 #include <vector>
 #include "../src/route_model.h"
 #include "../src/route_planner.h"
+#include "gtest/gtest.h"
 
-
-static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
-{   
+static std::optional<std::vector<std::byte>> ReadFile(const std::string& path)
+{
     std::ifstream is{path, std::ios::binary | std::ios::ate};
-    if( !is )
+    if (!is)
         return std::nullopt;
-    
+
     auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
-    
+    std::vector<std::byte> contents(size);
+
     is.seekg(0);
     is.read((char*)contents.data(), size);
 
-    if( contents.empty() )
+    if (contents.empty())
         return std::nullopt;
     return std::move(contents);
 }
 
-std::vector<std::byte> ReadOSMData(const std::string &path) {
+std::vector<std::byte> ReadOSMData(const std::string& path)
+{
     std::vector<std::byte> osm_data;
     auto data = ReadFile(path);
-    if( !data ) {
+    if (!data)
+    {
         std::cout << "Failed to read OSM data." << std::endl;
-    } else {
+    }
+    else
+    {
         osm_data = std::move(*data);
     }
     return osm_data;
@@ -39,13 +42,14 @@ std::vector<std::byte> ReadOSMData(const std::string &path) {
 //   Beginning RoutePlanner Tests.
 //--------------------------------//
 
-class RoutePlannerTest : public ::testing::Test {
+class RoutePlannerTest : public ::testing::Test
+{
   protected:
     std::string osm_data_file = "../map.osm";
     std::vector<std::byte> osm_data = ReadOSMData(osm_data_file);
     RouteModel model{osm_data};
     RoutePlanner route_planner{model, 10, 10, 90, 90};
-    
+
     // Construct start_node and end_node as in the model.
     float start_x = 0.1;
     float start_y = 0.1;
@@ -60,19 +64,21 @@ class RoutePlannerTest : public ::testing::Test {
     RouteModel::Node* mid_node = &model.FindClosestNode(mid_x, mid_y);
 };
 
-
 // Test the CalculateHValue method.
-TEST_F(RoutePlannerTest, TestCalculateHValue) {
+TEST_F(RoutePlannerTest, TestCalculateHValue)
+{
     EXPECT_FLOAT_EQ(route_planner.CalculateHValue(start_node), 1.1329799);
     EXPECT_FLOAT_EQ(route_planner.CalculateHValue(end_node), 0.0f);
     EXPECT_FLOAT_EQ(route_planner.CalculateHValue(mid_node), 0.58903033);
 }
 
-
-
 // Test the AddNeighbors method.
-bool NodesSame(RouteModel::Node* a, RouteModel::Node* b) { return a == b; }
-TEST_F(RoutePlannerTest, TestAddNeighbors) {
+bool NodesSame(RouteModel::Node* a, RouteModel::Node* b)
+{
+    return a == b;
+}
+TEST_F(RoutePlannerTest, TestAddNeighbors)
+{
     route_planner.AddNeighbors(start_node);
 
     // Correct h and g values for the neighbors of start_node.
@@ -82,7 +88,8 @@ TEST_F(RoutePlannerTest, TestAddNeighbors) {
     EXPECT_EQ(neighbors.size(), 4);
 
     // Check results for each neighbor.
-    for (int i = 0; i < neighbors.size(); i++) {
+    for (int i = 0; i < neighbors.size(); i++)
+    {
         EXPECT_PRED2(NodesSame, neighbors[i]->parent, start_node);
         EXPECT_FLOAT_EQ(neighbors[i]->g_value, start_neighbor_g_vals[i]);
         EXPECT_FLOAT_EQ(neighbors[i]->h_value, start_neighbor_h_vals[i]);
@@ -90,9 +97,9 @@ TEST_F(RoutePlannerTest, TestAddNeighbors) {
     }
 }
 
-
 // Test the ConstructFinalPath method.
-TEST_F(RoutePlannerTest, TestConstructFinalPath) {
+TEST_F(RoutePlannerTest, TestConstructFinalPath)
+{
     // Construct a path.
     mid_node->parent = start_node;
     end_node->parent = mid_node;
@@ -106,9 +113,9 @@ TEST_F(RoutePlannerTest, TestConstructFinalPath) {
     EXPECT_FLOAT_EQ(end_node->y, path.back().y);
 }
 
-
 // Test the AStarSearch method.
-TEST_F(RoutePlannerTest, TestAStarSearch) {
+TEST_F(RoutePlannerTest, TestAStarSearch)
+{
     route_planner.AStarSearch();
     EXPECT_EQ(model.path.size(), 33);
     RouteModel::Node path_start = model.path.front();
